b236edd15741efc30bef192f7c7da66d8ab85907
Rajasudhangowda321 <69045108+Rajasudhangowda321@users.noreply.github.com>
2020-10-17 16:56:05 +0530

    Delete Assignment3.m

--- Assignment3.m
+++
@@ -1,4 +0,0 @@
-%1. Create a vector of all the odd positive integers smaller than 100 in increasing order and save it into variable odds.
-%2. Create a vector of all the even positive integers smaller than or equal to 100 in decreasing order and save it into variable evens.
-odds=1:2:100%it is in increasing order
-evens=100:-2:2%it is in decreasing order
